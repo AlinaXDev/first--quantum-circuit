ff70c673ac88989be20b8a3fcecf48ac8be0e837
Alina <202512736@students.asu.edu.jo>
2026-09-07 19:09:02 +0300

    Add circuit diagram and results histogram

--- first_circuit.py
+++ first_circuit.py
@@ -12,16 +12,33 @@
 
 from qiskit import QuantumCircuit
 from qiskit_aer import AerSimulator
+from qiskit.visualization import plot_histogram
 
+# Build the circuit: 2 qubits, 2 classical bits to store measurement results
 qc = QuantumCircuit(2, 2)
-# Put qubit 0 into superposition (mix of 0 and 1)
-qc.h(0)
-# Entangle qubit 0 with qubit 1 - measuring one affects the other
-qc.cx(0, 1)
-qc.measure([0,1], [0,1])
 
+# Put qubit 0 into superposition (a mix of 0 and 1 at the same time)
+qc.h(0)
+
+# Entangle qubit 0 with qubit 1 - measuring one instantly affects the other
+qc.cx(0, 1)
+
+# Measure both qubits and store results in the classical bits
+qc.measure([0, 1], [0, 1])
+
+# Print an ASCII diagram of the circuit to the terminal
 print(qc.draw())
- 
+
+# Save a proper image of the circuit diagram
+qc.draw(output='mpl', filename='circuit.png')
+
+# Run the circuit on a local simulator (pretends to be a quantum computer)
 sim = AerSimulator()
 result = sim.run(qc).result()
-print(result.get_counts())
+counts = result.get_counts()
+
+# Print the raw measurement counts
+print(counts)
+
+# Save a histogram image of the results
+plot_histogram(counts, filename='results.png')
